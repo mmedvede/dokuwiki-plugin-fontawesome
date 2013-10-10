b15bf5a65a7e0f3e1fc19efe49a5726cbb729c8d
Mikhail Medvedev <mmedvede@cs.uml.edu>
2013-10-09 21:21:30 -0400

    initial, dokuwiki wizard

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the fontawesome plugin
+ *
+ * @author Mikhail Medvedev <mmedvede@cs.uml.edu>
+ */
+
+//$conf['fixme']    = 'FIXME';
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the fontawesome plugin
+ *
+ * @author Mikhail Medvedev <mmedvede@cs.uml.edu>
+ */
+
+
+//$meta['fixme'] = array('string');
+
